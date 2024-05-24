90f29c6ede0aca1e74a7c7d62ceb0c5d42c506ca
Arun Kushwaha <121769639+Arun-kushwaha007@users.noreply.github.com>
2024-05-24 18:44:49 +0530

    register

--- public/src/App.js
+++ public/src/App.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Chat from './pages/Chat';
 
 export default function App() {
   return (
-    <div>App</div>
-  )
+  <BrowserRouter>
+    <Routes>
+      <Route path='/register' element={<Register/>} />
+      <Route path='/login' element={<Login/>} />
+      <Route path='/' element={<Chat/>} />
+    </Routes>
+  </BrowserRouter>
+  );
 }
