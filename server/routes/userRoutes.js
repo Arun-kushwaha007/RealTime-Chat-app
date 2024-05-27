2dcf984327ad2dae2f2c66bebd3e2a69ccb69b40
Arun Kushwaha <121769639+Arun-kushwaha007@users.noreply.github.com>
2024-05-27 20:50:30 +0530

    set avatar.

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -4,5 +4,7 @@ const router = require("express").Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.post("/setAvatar/:id", setAvatar);
+
 
 module.exports = router;
