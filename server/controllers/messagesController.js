8a66a8ac747cb8deb7f0ff0cc9dad432e4be7fcd
Arun Kushwaha <121769639+Arun-kushwaha007@users.noreply.github.com>
2024-06-02 20:46:27 +0530

    message component.

--- server/controllers/messagesController.js
+++ server/controllers/messagesController.js
@@ -15,4 +15,10 @@ module.exports.addMessage = async(req, res, next) =>{
     }
 };
 
-module.exports.getAllMessage = async(req, res, next) =>{};
+module.exports.getAllMessage = async(req, res, next) =>{
+    try{
+
+    }catch(ex){
+        next(ex);
+    }
+};
